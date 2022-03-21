c74a6224a32bf60ea2c3ff455fb903df8c033011
babymonie <62741287+babymonie@users.noreply.github.com>
2022-03-21 16:39:33 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -94,44 +94,47 @@ end)
 --Silent Aim Toggle
 aim:NewToggle("Silent Aim", "Toggle Silent Aim", true, function(b)
     if b then
+        --> VARIABLES <--
         local plrs = game:GetService("Players")
         local plr = plrs.LocalPlayer
         local mouse = plr:GetMouse()
         local camera = game:GetService("Workspace").CurrentCamera
         
+        --> FUNCTIONS <--
         function notBehindWall(target)
             local ray = Ray.new(plr.Character.Head.Position, (target.Position - plr.Character.Head.Position).Unit * 300)
-            local part, position = game:GetService("Workspace"):FindPartOnRayWithIgnoreList(ray, {plr.Character},false,true)
+            local part, position = game:GetService("Workspace"):FindPartOnRayWithIgnoreList(ray, {plr.Character}, false, true)
             if part then
                 local humanoid = part.Parent:FindFirstChildOfClass("Humanoid")
                 if not humanoid then
                     humanoid = part.Parent.Parent:FindFirstChildOfClass("Humanoid")
                 end
-                if humanoid and target and humanoid.Parent ~= target.Parent then
-                    local pos, visible = camera:WorldToScreenPoint(part.Position)
+                if humanoid and target and humanoid.Parent == target.Parent then
+                    local pos, visible = camera:WorldToScreenPoint(target.Position)
                     if visible then
                         return true
                     end
                 end
             end
         end
+        
         function getPlayerClosestToMouse()
             local target = nil
             local maxDist = 100
             for _,v in pairs(plrs:GetPlayers()) do
                 if v.Character then
                     if v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health ~= 0 and v.Character:FindFirstChild("HumanoidRootPart") and v.TeamColor ~= plr.TeamColor then
-                        local pos, vis = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position) 
+                        local pos, vis = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
                         local dist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(pos.X, pos.Y)).magnitude
                         if dist < maxDist and vis then
-                           local torsoPos = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
-                           local torsoDist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(torsoPos.X, torsoPos.Y)).magnitude
-                           local headPos = camera:WorldToViewportPoint(v.Character.Head.Position)
-                           local headDist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(headPos.X, headPos.Y)).magnitude
-                            if torsoDist < headDist then
-                              if notBehindWall(v.Character.Head) then
-                                target = v.Character.Head
-                              end
+                            local torsoPos = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
+                            local torsoDist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(torsoPos.X, torsoPos.Y)).magnitude
+                            local headPos = camera:WorldToViewportPoint(v.Character.Head.Position)
+                            local headDist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(headPos.X, headPos.Y)).magnitude
+                            if torsoDist > headDist then
+                                if notBehindWall(v.Character.Head) then
+                                    target = v.Character.Head
+                                end
                             else
                                 if notBehindWall(v.Character.HumanoidRootPart) then
                                     target = v.Character.HumanoidRootPart
@@ -140,13 +143,45 @@ aim:NewToggle("Silent Aim", "Toggle Silent Aim", true, function(b)
                             maxDist = dist
                         end
                     end
-                end  
+                end
             end
             return target
         end
         
-    else
+        --> Hooking to the remote <--
+        local gmt = getrawmetatable(game)
+        setreadonly(gmt, false)
+        local oldNamecall = gmt.__namecall
         
+        gmt.__namecall = newcclosure(function(self, ...)
+            local Args = {...}
+            local method = getnamecallmethod()
+            if tostring(self) == "HitPart" and tostring(method) == "FireServer" then
+                Args[1] = getPlayerClosestToMouse()
+                Args[2] = getPlayerClosestToMouse().Position
+                return self.FireServer(self, unpack(Args))
+            end
+            return oldNamecall(self, ...)
+        end)
+    else
+        local gmt = getrawmetatable(game)
+        setreadonly(gmt, false)
+        gmt.__namecall = oldNamecall
+    end
+end)
+--Aimbot
+aim:NewToggle("Aimbot", "Toggle Aimbot", true, function(b)
+    if b then
+        function getClosest()
+            local closestDistance
+            local closestPlayer
+            for i,v in pairs(game.Players:GetChildren()) do
+                if v == game.Players.LocalPlayer and v.Team == game.Players.LocalPlayer.Team then
+                    local distance = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position )
+                end
+            end
+        end
+    else
     end
 end)
 --Wallbang
